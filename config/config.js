f54e9e4be5547d46a0f94efb454b279f75f859aa
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-05-21 17:01:12 +0700

    aman aja

--- config/config.js
+++ config/config.js
@@ -32,7 +32,7 @@ module.exports = {
     username: `${process.env.DB_USERNAME}`,
     password: `${process.env.DB_PASSWORD}`,
     database: `${process.env.DB_NAME}`,
-    host: `${process.env.DB_PASSWORD}`,
+    host: `${process.env.PROD_DB_HOST}`,
     port: `${process.env.DB_PORT}`,
     dialect: "postgres",
     dialectOptions: {
