122e6ac54cb1cb0a3565d1e9dd435f30ccc29c4b
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-05-21 16:53:52 +0700

    update config port

--- config/config.js
+++ config/config.js
@@ -33,6 +33,7 @@ module.exports = {
     password: `${process.env.DB_PASSWORD}`,
     database: `${process.env.DB_NAME}`,
     host: `${process.env.DB_PASSWORD}`,
+    port: `${process.env.DB_PORT}`,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
