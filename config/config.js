b72f873939a728b2be83703cd3a0d6938957c95a
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-05-21 16:34:13 +0700

    changes

--- config/config.js
+++ config/config.js
@@ -29,10 +29,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.PROD_DB_HOST,
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    host: `${process.env.PROD_DB_HOST}`,
     dialect: "postgres",
     dialectOptions: {
       useUTC: false, //for reading from database
