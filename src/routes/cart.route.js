fdd0e9c050093514e2d543e5f153c04b4cd164cc
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-05-27 11:35:45 +0700

    remove console.log

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,12 +1,15 @@
-const router = require('express').Router();
-const cartController = require('./../controllers/cart.controller');
-const verifyCust = require('../middlewares/verifyCust.middleware');
+const router = require("express").Router();
+const cartController = require("./../controllers/cart.controller");
+const verifyCust = require("../middlewares/verifyCust.middleware");
 
-router.get('/count', verifyCust,  cartController.countProductInCart);
-router.post('/add', verifyCust, cartController.addProductInCart);
-router.get('/', verifyCust, cartController.getProductInCart);
-router.put('/update/products', verifyCust, cartController.updateQtyInCart);
-router.delete('/remove/products/:ProductId', cartController.removeProductInCart);
-
+router.get("/count", verifyCust, cartController.countProductInCart);
+router.post("/add", verifyCust, cartController.addProductInCart);
+router.get("/", verifyCust, cartController.getProductInCart);
+router.put("/update/products", verifyCust, cartController.updateQtyInCart);
+router.delete(
+  "/remove/products/:ProductId",
+  verifyCust,
+  cartController.removeProductInCart
+);
 
 module.exports = router;
